966b41d9b0911382996b22b754c24e4a32b48462
Tobias Helfenstein <tobias@die-softwarezimmerei.de>
2024-09-04 18:58:27 +0200

    ProgressBar in MainWindow integrated

--- Jagdorganisation.xaml.cs
+++ Jagdorganisation.xaml.cs
@@ -17,6 +17,9 @@ using System.Collections;
 using System.Runtime.InteropServices;
 using xl = Microsoft.Office.Interop.Excel;
 
+using System.Threading;
+using System.ComponentModel;
+
 namespace Jagdorganisation
 {
     /// <summary>
@@ -30,9 +33,45 @@ namespace Jagdorganisation
             InitializeComponent();
         }
 
+        struct ProcessData
+        {
+            public string Filename;
+            public List<string> Checkboxes;
+            public bool? Separator;
+        }
+
         private void CloseButton_Click(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+
+        private void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            (sender as BackgroundWorker).ReportProgress(10, "Einteilungsdatei wird verarbeitet");
+            _printer = new HunterGroupPrinter();
+            _printer.CreateCardsFromSource(((ProcessData)e.Argument).Filename);
+
+            int progress = (100-20)/((ProcessData)e.Argument).Checkboxes.Count;
+            for (int i = 0; i < ((ProcessData)e.Argument).Checkboxes.Count; i++)
+            {
+                (sender as BackgroundWorker).ReportProgress(10 + (i + 1) * progress, ((ProcessData)e.Argument).Checkboxes[i] + " werden gedruckt");
+                PrintGroups(((ProcessData)e.Argument).Checkboxes[i], ((ProcessData)e.Argument).Separator);
+            }
+
+            (sender as BackgroundWorker).ReportProgress(100, "Fertig");
+        }
+
+        private void worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            ProgressBar.Value = e.ProgressPercentage;
+            StatusInfoText.Content = e.UserState.ToString();
+        }
+
+        private void worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            MessageBox.Show("Alle Gruppeneinteilungen wurden gedruckt!", "Jagdorganisation", MessageBoxButton.OK, MessageBoxImage.Information);
+            ProgressBar.Value = 0;
+            StatusInfoText.Content = "keine Einteilung geladen";
         }
 
         private void PrintButton_Click(object sender, RoutedEventArgs e)
@@ -58,33 +97,40 @@ namespace Jagdorganisation
                 return;
             }
 
-            _printer = new HunterGroupPrinter();
-            _printer.CreateCardsFromSource(source_file);
-
             // create array with checkboxes for printing groups
-            CheckBox[] check_boxes = new CheckBox[] {
+            ProcessData process_data;
+            process_data.Filename = source_file;
+            process_data.Checkboxes = new List<string>();
+            process_data.Separator = SeparatorCheckBox.IsChecked;
+            foreach(CheckBox box in new CheckBox[] {
                 LeaderCheckBox,
                 ShootersCheckBox,
                 DogsCheckBox,
                 ReservesCheckBox
-            };
+            })
+            {
+                if (box.IsChecked == true)
+                {
+                    process_data.Checkboxes.Add(box.Content.ToString());
+                }
+                
+            }
 
-            // define the print action
-            // then execute the print action for each array element
-            Action<CheckBox> print = new Action<CheckBox>(PrintGroups);
-            Array.ForEach(check_boxes, print);
+            BackgroundWorker worker = new BackgroundWorker();
+            worker.WorkerReportsProgress = true;
+            worker.DoWork += worker_DoWork;
+            worker.ProgressChanged += worker_ProgressChanged;
+            worker.RunWorkerCompleted += worker_RunWorkerCompleted;
+            worker.RunWorkerAsync(process_data);
         }
 
-        private void PrintGroups(CheckBox box)
+        private void PrintGroups(string group, bool? separator)
         {
-            // check, if the separator sheet should print
-            bool? separator = SeparatorCheckBox.IsChecked;
-
             // print out if the box is checked
-            if (box.IsChecked == true) { _printer.PrintCards(box.Content.ToString(), separator); }
+            _printer.PrintCards(group, separator);
 
             // timer before next print action
-
+            Thread.Sleep(30 * 1000);
         }
     }
 }
